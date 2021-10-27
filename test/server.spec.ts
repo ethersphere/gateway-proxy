681c8496b3211afbca46fb126b60594c426aa75c
Vojtech Simetka <vojtech@simetka.cz>
2021-10-27 17:50:40 +0200

    chore: added bee-js as dev dependency

--- test/server.spec.ts
+++ test/server.spec.ts
@@ -1,7 +1,9 @@
 import { createApp } from '../src/server'
 import request from 'supertest'
+import { Bee, BeeDebug } from '@ethersphere/bee-js'
 
 const BEE_API_URL = process.env.BEE_API_URL || 'http://localhost:1633'
+const BEE_DEBUG_API_URL = process.env.BEE_DEBUG_API_URL || 'http://localhost:1635'
 const BEE_API_URL_WRONG = process.env.BEE_API_URL_WRONG || 'http://localhost:2021'
 const AUTH_SECRET = process.env.AUTH_SECRET || 'super_secret_token'
 
@@ -9,6 +11,9 @@ const app = createApp({ BEE_API_URL })
 const appWrong = createApp({ BEE_API_URL: BEE_API_URL_WRONG })
 const appAuth = createApp({ BEE_API_URL, AUTH_SECRET })
 const appAuthWrong = createApp({ BEE_API_URL: BEE_API_URL_WRONG, AUTH_SECRET })
+
+const bee = new BeeDebug(BEE_DEBUG_API_URL)
+const beeDebug = new BeeDebug(BEE_DEBUG_API_URL)
 
 describe('GET /health', () => {
   it('should return 200 & OK', async () => {
@@ -58,5 +63,13 @@ describe('GET /readiness', () => {
     const res = await request(appAuth).get(`/readiness`).set('authorization', AUTH_SECRET).expect(200)
 
     expect(res.text).toEqual('OK')
+  })
+})
+
+describe('POST /bzz', () => {
+  it('should upload a file without replacing stamp header', async () => {
+    const batch = await beeDebug.createPostageBatch('1000000', 20)
+
+    console.log(batch) // eslint-disable-line
   })
 })
