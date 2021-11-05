65af70f506d387961df1800b75361ada5b034e53
Vojtech Simetka <vojtech@simetka.cz>
2021-11-05 08:43:38 +0100

    chore: add comments

--- src/stamps.ts
+++ src/stamps.ts
@@ -7,6 +7,11 @@ export interface PostageStamps {
   POSTAGE_USAGE?: number
 }
 
+/**
+ * Get postage stamp that should be replaced in a the proxy request header
+ *
+ * @throws Error if could not find suitable postage stamp
+ */
 export function getPostageStamp({ POSTAGE_STAMP }: PostageStamps = {}): string {
   const stamp = POSTAGE_STAMP
 
@@ -15,6 +20,9 @@ export function getPostageStamp({ POSTAGE_STAMP }: PostageStamps = {}): string {
   throw new Error('No postage stamp')
 }
 
+/**
+ * Return true if the proxy should replace the postage stamp header
+ */
 export function shouldReplaceStamp({ POSTAGE_STAMP }: PostageStamps = {}): boolean {
   return Boolean(POSTAGE_STAMP)
 }
