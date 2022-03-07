5cd0e6172072590a0c799f0bd24cdf5e4ccd0276
Vojtech Simetka <vojtech@simetka.cz>
2022-03-07 09:12:25 +0100

    test: use variable for the post endpoints being proxied

--- test/header-check.mockserver.ts
+++ test/header-check.mockserver.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import type { Server } from 'http'
+import { POST_PROXY_ENDPOINTS } from '../src/server'
 
 // This simple server just sends back the request headers as response body
 export async function createHeaderCheckMockServer(): Promise<Server> {
   const app = express()
 
-  app.post(['/bzz', '/bytes', '/chunks', '/feeds/:owner/:topic', '/soc/:owner/:id'], (req, res) => {
+  app.post(POST_PROXY_ENDPOINTS, (req, res) => {
     res.send(JSON.stringify(req.headers))
   })
 
