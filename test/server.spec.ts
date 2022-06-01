820a340c79df4f286fd17400c1606a1ca82510c1
Vojtech Simetka <vojtech@simetka.cz>
2022-06-01 16:58:17 +0200

    refactor: use wait for stamp to be usable from bee-js

--- test/server.spec.ts
+++ test/server.spec.ts
@@ -255,7 +255,7 @@ describe('POST /chunks', () => {
     expect(reference).toEqual(hash)
 
     const downloadedData = await bee.downloadChunk(reference)
-    expect(downloadedData).toEqual(data)
+    expect(downloadedData.toString()).toEqual(data.toString())
   })
 
   it('should store and retrieve chunk with environment defined postage stamp', async () => {
@@ -271,7 +271,7 @@ describe('POST /chunks', () => {
     expect(reference).toEqual(hash)
 
     const downloadedData = await bee.downloadChunk(reference)
-    expect(downloadedData).toEqual(data)
+    expect(downloadedData.toString()).toEqual(data.toString())
   })
 })
 
@@ -289,7 +289,7 @@ describe('GET /chunks/:reference/', () => {
     expect(reference).toEqual(hash)
 
     const downloadedData = await beeProxy.downloadChunk(reference)
-    expect(downloadedData).toEqual(data)
+    expect(downloadedData.toString()).toEqual(data.toString())
   })
 
   it('with authorization enabled should return 403 & Forbidden', async () => {
