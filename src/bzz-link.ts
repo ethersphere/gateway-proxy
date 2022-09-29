56122ea0508409aeb91775a64a4d4cdafa3d8b68
Cafe137 <xcafe137@gmail.com>
2022-09-29 13:06:45 +0200

    chore: revert import sorting

--- src/bzz-link.ts
+++ src/bzz-link.ts
@@ -1,5 +1,5 @@
-import * as swarmCid from '@ethersphere/swarm-cid'
 import { Request, Response } from 'express'
+import * as swarmCid from '@ethersphere/swarm-cid'
 import { logger } from './logger'
 
 export class NotEnabledError extends Error {}
