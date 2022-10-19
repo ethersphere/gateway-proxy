6eb466746857830570db13536bb758382afcf1f6
Adam Uhlíř <adam@uhlir.dev>
2022-10-19 12:17:11 +0200

    chore: more esm tweaks

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,9 +2,8 @@
  * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/en/configuration.html
  */
-import type { Config } from '@jest/types'
 
-export default async (): Promise<Config.InitialOptions> => {
+export default async () => {
   return {
     preset: 'ts-jest',
     testEnvironment: 'node',
