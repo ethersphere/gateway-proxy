bdb5d67af4c1fd3d5ff47041af0d6aa7d953e82d
Cafe137 <xcafe137@gmail.com>
2026-04-21 14:09:38 +0200

    fix: update vulnerable dependencies

--- src/server.ts
+++ src/server.ts
@@ -160,7 +160,7 @@ export const createApp = (
           throw Error('Homepage not available')
         }
         const contentType = response.headers['content-type']
-        res.set('content-type', contentType || 'application/octet-stream')
+        res.set('content-type', contentType ? contentType.toString() : 'application/octet-stream')
         res.send(await response.data)
 
         return
